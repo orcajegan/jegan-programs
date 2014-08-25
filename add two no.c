0cf209ac1e26251641b19bbcbbe624c1914c7c41
jegan <jegang91@gmail.com>
2014-08-25 04:31:06 -0700

    Ch3Example4

---
+++ add two no.c
@@ -0,0 +1,17 @@
+/* Displaying Variable */
+/* jegan, aug 2014 */
+#include<stdio.h>
+
+ int main (void)
+ 
+{
+    int sum ;
+    
+   
+    sum=50+51;
+    printf("sum of the two number %i\n",sum);
+
+    return 0;
+
+}
+   
