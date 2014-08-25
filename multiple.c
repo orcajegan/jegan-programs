c8c80690b4493109c53ab37ac5584e83e8c58ede
jegan <jegang91@gmail.com>
2014-08-25 05:05:36 -0700

    Ch3Example5

---
+++ multiple.c
@@ -0,0 +1,19 @@
+/* Displaying Multiple values */
+/* jegan, aug 2014 */
+
+#include<stdio.h>
+
+ int main (void)
+ 
+{
+    int value1, value2, sum ;
+    
+    value1=50;
+    value2=100;
+    sum=value1+value2;
+    printf("sum of %i and %i is %i\n",value1,value2,sum);
+
+    return 0;
+
+}
+   
