d898ca5fc283260573e67e9041d678dd8940d070
jegan <jegang91@gmail.com>
2014-09-16 04:06:39 -0700

    Chapter-7 Examples

---
+++ C7EXAMPLE-6.c
@@ -0,0 +1,15 @@
+/* program to character arrays
+   jegan september 2014 */
+
+int main (void)
+{
+	int i ;
+	char word[] = { 'H', 'e', 'l', 'l', 'o', '!'};
+	
+	for (i = 0; i < 6; ++i)
+	printf("%c", word[i]);
+	
+	printf("\n");
+	
+	return 0;
+}
