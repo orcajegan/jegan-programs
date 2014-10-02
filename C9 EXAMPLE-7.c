7fae6e56f529d90ee0de09e813f706a8d199f47f
jegan <jegang91@gmail.com>
2014-10-02 00:18:30 -0700

    C9 Examples

---
+++ C9 EXAMPLE-7.c
@@ -0,0 +1,29 @@
+/* program to illustrate the structure and arrays
+ * jegan october 2014 */
+ # include <stdio.h>
+ int main (void)
+ {
+ 	int i;
+ 	
+ 	struct month
+ 	{
+ 		int numberofdays;
+ 		char name[3];
+  	};
+ const struct month months[12] =
+   { { 31, {'J', 'a', 'n'} }, { 28, {'F', 'e', 'b'} },
+   { 31, {'M', 'a', 'r'} }, { 30, {'A', 'p', 'r'} },
+   { 31, {'M', 'a', 'y'} }, { 30, {'J', 'u', 'n'} },
+   { 31, {'J', 'u', 'l'} }, { 31, {'A', 'u', 'g'} },
+   { 30, {'S', 'e', 'p'} }, { 31, {'O', 'c', 't'} },
+   { 30, {'N', 'o', 'v'} }, { 31, {'D', 'e', 'c'} } };	
+ 
+ printf ("Month Number of Days\n");
+ printf ("--------------------\n");
+ 
+ for (i = 0; i < 12; ++i)
+   printf ("%c%c%c       %i\n",
+   months[i].name[0],months[i].name[1],
+   months[i].name[2],months[i].numberofdays);
+ return 0;
+ } 
